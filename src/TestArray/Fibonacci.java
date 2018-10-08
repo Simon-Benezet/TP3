1d13f4f982181beca0bbc8f6b0262a7d0bfa93ac
Simon-Benezet <43878293+Simon-Benezet@users.noreply.github.com>
2018-10-08 10:00:58 +0200

    a

--- src/TestArray/Fibonacci.java
+++ src/TestArray/Fibonacci.java
@@ -2,14 +2,15 @@ package TestArray;
 import java.util.Scanner;
 
 /**
- * @author formation
- *
+ * @author Simon Benezet
+ * Calcul du nombre dans la suite de Fibonacci au rang N
  */
 public class Fibonacci 
 {
 	public static void main(String[] args)
 	{
 		int val1 = 0, val2 = 1, val3 = 0;
+		// Choisir le rang 
 		System.out.println("Entrez N");
 		Scanner a = new Scanner(System.in);
 		int N = a.nextInt();
