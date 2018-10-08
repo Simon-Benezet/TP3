52f28c043d8a6c2e09a187c009c9095827e98ff2
Simon-Benezet <43878293+Simon-Benezet@users.noreply.github.com>
2018-10-08 09:53:10 +0200

    a

--- src/TestArray/Fibonacci.java
+++ src/TestArray/Fibonacci.java
@@ -2,17 +2,19 @@ package TestArray;
 import java.util.Scanner;
 
 /**
- * @author formation
- *
+ * @author Simon Benezet
+ *	Calcul de Fibonacci
  */
 public class Fibonacci 
 {
 	public static void main(String[] args)
 	{
 		int val1 = 0, val2 = 1, val3 = 0;
+		//Entrer la valeur de rang N
 		System.out.println("Entrez N");
 		Scanner a = new Scanner(System.in);
 		int N = a.nextInt();
+		
 		for(int i = 0; i < N; i++)
 		{
 			val3 = val1 + val2;
